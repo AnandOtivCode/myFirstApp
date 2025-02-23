bae1f29f05b655ea5c4bf33c8ebe0df35a8c0962
Anand Otiv <w0858857@myscc.ca>
2025-02-22 19:59:13 -0500

    Initial Commit

---
+++ myFirstApp/myFirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  myFirstAppApp.swift
+//  myFirstApp
+//
+//  Created by Anand Otiv on 2025-02-22.
+//
+
+import SwiftUI
+
+@main
+struct myFirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
