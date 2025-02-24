89425641ff7bf70f92aae61325f326579c4f4c57
Anand Otiv <w0858857@myscc.ca>
2025-02-24 00:36:54 -0500

    Added ZStack to wrap around VStack Added a custom cyan like color for a background

--- myFirstApp/ContentView.swift
+++ myFirstApp/ContentView.swift
@@ -8,30 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    
     var body: some View {
-        VStack(alignment: .trailing) {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Anand Otiv")
-            //Changes Font
-                .font(.subheadline)
-            //Changes Font Weight
-                .fontWeight(.bold)
-            //Changes Font Color
-                .foregroundColor(Color.purple)
-            //Alligns Text in the centre
-                .multilineTextAlignment(.center)
+        
+        ZStack{
             
-            Image("BlueJay")
-            //Makes the image resizable
-                .resizable(
-                )
-            //Fits the image in the width of the phone
-                .aspectRatio(contentMode: .fit).clipShape(RoundedRectangle(cornerRadius: 123.2))//.clipShape(Circle())
+            
+            VStack(alignment: .trailing) {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                Text("Anand Otiv")
+                //Changes Font
+                    .font(.subheadline)
+                //Changes Font Weight
+                    .fontWeight(.bold)
+                //Changes Font Color
+                    .foregroundColor(Color.purple)
+                //Alligns Text in the centre
+                    .multilineTextAlignment(.center)
                 
+                Image("BlueJay")
+                //Makes the image resizable
+                    .resizable(
+                    )
+                //Fits the image in the width of the phone
+                    .aspectRatio(contentMode: .fit).clipShape(RoundedRectangle(cornerRadius: 123.2))//.clipShape(Circle())
+                
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
